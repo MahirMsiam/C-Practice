1b2242b7318e4351ccf0f297a80211e7bad86eb4
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2024-04-15 22:51:10 +0600

    Update BeautifulMatrix.cpp

--- ProblemSet/BeautifulMatrix.cpp
+++ ProblemSet/BeautifulMatrix.cpp
@@ -6,6 +6,13 @@ int main()
     int a[5][5];
     int x, y;
     for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 1) {
+                x = i;
+                y = j;
+            }
+        }
         
     }
     
