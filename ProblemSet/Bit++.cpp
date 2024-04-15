f53fcfeadacace3eb31b87737f5e8b7b471bf936
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2024-04-15 15:51:11 +0600

    Update Bit++.cpp

--- ProblemSet/Bit++.cpp
+++ ProblemSet/Bit++.cpp
@@ -2,8 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
-
+{   
+int n;
+cin >> n;
+int x = 0;
+for (int i = 0; i < n; i++) {
+    string op;
+    cin >> op;
+    if (op == "++X" || op == "X++") {
+        x++;
+    } else {
+        x--;
+    }
+}
 cout << x << endl;
 
     return 0;
