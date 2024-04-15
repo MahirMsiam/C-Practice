c02e3ae519990d7a37fe30fb1dd50f4124d0bf48
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2024-04-15 22:54:17 +0600

    Update Petya&Strings.cpp

--- ProblemSet/Petya&Strings.cpp
+++ ProblemSet/Petya&Strings.cpp
@@ -5,6 +5,11 @@ int main()
 {
     string s1, s2;
     cin >> s1 >> s2;
+    for (int i = 0; i < s1.size(); i++)
+    {
+        s1[i] = tolower(s1[i]);
+        s2[i] = tolower(s2[i]);
+    }
 
 
 
