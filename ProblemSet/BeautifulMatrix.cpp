20f89c6b7baf1aeb9cabef6deb9fadcf1b35774e
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2024-04-15 22:51:23 +0600

    Update BeautifulMatrix.cpp

--- ProblemSet/BeautifulMatrix.cpp
+++ ProblemSet/BeautifulMatrix.cpp
@@ -1,22 +1,22 @@
-//Beautiful Matrix Problem Codeforces
-#include<bits/stdc++.h>
+// Beautiful Matrix Problem Codeforces
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int a[5][5];
     int x, y;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
             cin >> a[i][j];
-            if (a[i][j] == 1) {
+            if (a[i][j] == 1)
+            {
                 x = i;
                 y = j;
             }
         }
-        
     }
-    
-    
+
     return 0;
 }
-
