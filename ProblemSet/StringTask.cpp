d524162f5a716c5e9640f98366937906ab96a34a
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2024-04-17 22:40:19 +0600

    Update StringTask.cpp

--- ProblemSet/StringTask.cpp
+++ ProblemSet/StringTask.cpp
@@ -5,5 +5,17 @@ int main()
 {
     string s;
     cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y')
+        {
+            continue;
+        }
+        else
+        {
+            cout << ".";
+            cout << (char)tolower(s[i]);
+        }
+    }
     
 }
